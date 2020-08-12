d1a04b6b1e02f8ee5783ccfa1c4efd54ce77f88e
skrvasilis <skrvasilis@gmail.com>
2020-08-12 13:27:09 +0200

    fixed

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.scss'
+
+
+export default function App(){ 
+    let name = 'world'
+    
+        return (
+            <React.Fragment>
+            <h1 style={{'text-align':'center'}}>Hello  {name} </h1>
+            
+            </React.Fragment>
+        )
+    }
+
